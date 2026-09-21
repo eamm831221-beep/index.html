b759f425a2a72a4053040a32ef52171e815ed556
eamm831221-beep <eamm831221@gmail.com>
2026-09-21 15:41:46 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
+const CACHE_VCXC = 'vcxc-cache-v1';
+const RECURSOS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VCXC).then((cache) => {
+      return cache.addAll(RECURSOS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
 
